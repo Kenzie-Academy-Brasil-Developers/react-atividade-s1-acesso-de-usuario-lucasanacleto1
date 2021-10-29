a1a2919e8ed7778e9f1c25d7da6f221c616dcac5
Lucas Anacleto <lucasanacleto@outlook.com.br>
2021-10-29 16:22:44 -0300

    fixing text h1

--- src/components/RestrictedPage/index.jsx
+++ src/components/RestrictedPage/index.jsx
@@ -6,14 +6,14 @@ const RestrictedPage = ({ isLoggedIn, user, Login, Logout }) => {
     <div>
       {isLoggedIn ? (
         <div>
-          <h1>Bem vindo {user}</h1>
+          <h1>Bem vindo, {user}!</h1>
           <button className="btn" onClick={Logout}>
             Logout
           </button>
         </div>
       ) : (
         <div>
-          <h2>Você não pode acessar essa página</h2>
+          <h2>Você não pode acessar essa página.</h2>
           <button className="btn" onClick={Login}>
             Login
           </button>
